1d518b160f4cba62f5a304713b20b47e69189c74
Luis Specian <lspecian@gmail.com>
2025-08-10 17:23:49 +0200

    remove rust stuff

--- data-ingestion/mtg_json/target/debug/deps/mtg_json-e6759cdd9c081e8b.d
+++
@@ -1,5 +0,0 @@
-/Users/luis/Development/mtg/data-ingestion/mtg_json/target/debug/deps/mtg_json-e6759cdd9c081e8b: src/main.rs
-
-/Users/luis/Development/mtg/data-ingestion/mtg_json/target/debug/deps/mtg_json-e6759cdd9c081e8b.d: src/main.rs
-
-src/main.rs:
--- data-ingestion/mtg_json/target/debug/mtg_json.d
+++
@@ -1,1 +0,0 @@
-/Users/luis/Development/mtg/data-ingestion/mtg_json/target/debug/mtg_json: /Users/luis/Development/mtg/data-ingestion/mtg_json/src/main.rs
